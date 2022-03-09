961de510c47abbc3372c01ced89e485f0173568c
Logan Koshenka <koshenka27@gmail.com>
2022-03-09 18:09:24 -0500

    Initial Commit

---
+++ GameboyTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameboyTutorial
+//
+//  Created by Logan Koshenka on 3/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameboyTutorial/GameboyTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameboyTutorialApp.swift
+//  GameboyTutorial
+//
+//  Created by Logan Koshenka on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GameboyTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
